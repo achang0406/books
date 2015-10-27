036ba3387cd76ff0eea20509808431ee12b1f1fe
Austin Chang <achang0406@gmail.com>
2015-10-27 11:37:00 -0700

    edited login

--- application/views/login_registration.php
+++ application/views/login_registration.php
@@ -2,33 +2,14 @@
 <head>
 	<title>Login&Registration</title>
 	<style type="text/css">
-		*{
-		padding: 0;
-		margin: 0;
-		}
-		#container{
-			padding: 10px 30px;
-		}
-		#registration, #login{
-			display: inline-block;
-			vertical-align: top;
-			margin: 50px;
-		}
 		form{
 			border: 1px solid black;
 			display: inline-block;
 			padding: 20px;
+			width: 100%;
 		}
-		form label{
-			display: block;
-			margin: 20px 0px;
-		}
-		form span{
-			width: 150px;
-			display: inline-block;
-		}
-		form button{
-			float: right;
+		.row{
+			margin-top: 100px;
 		}
 		.error{
 			color: red;
@@ -38,23 +19,50 @@
 	<script type="text/javascript" src=""https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js></script>
 </head>
 <body>
-	<div id='container'>
-		<div id='registration'>
-			<form action='/processes/register' method='post'>
-				<label><span>Name:</span><input type='text' name='name' placeholder='name'><?php if($this->session->flashdata('name')){echo '<span class="error">'.$this->session->flashdata('name').'</span>';}?></label>
-				<label><span>Alias:</span><input type='text' name='alias' placeholder='alias'><?php if($this->session->flashdata('alias')){echo '<span class="error">'.$this->session->flashdata('alias').'</span>';}?></label>
-				<label><span>Email:</span><input type='text' name='email' placeholder='email'><?php if($this->session->flashdata('email')){echo '<span class="error">'.$this->session->flashdata('email').'</span>';}?></label>
-				<label><span>Password:</span><input type='password' name='password'><?php if($this->session->flashdata('password')){echo '<span class="error">'.$this->session->flashdata('password').'</span>';}?></label>
-				<label><span>Confirm Password:</span><input type='password' name='confirm_password' ><?php if($this->session->flashdata('confirm_password')){echo '<span class="error">'.$this->session->flashdata('confirm_password').'</span>';}?></label>
-				<button type='submit'>Register</button>
-			</form>
-		</div>
-		<div id='login'>
-			<form action='/processes/login' method='post'>
-				<label><span>Email:</span><input type='text' name='email' placeholder='email'></label>
-				<label><span>Password:</span><input type='password' name='password'></label>
-				<button type='submit'>Login</button>
-			</form>
+	<div id='container' class="container-fluid">
+		<div class="row">
+			<div id='registration' class="col-sm-5 col-sm-offset-1">
+				<form action='/processes/register' method='post'>
+					<div class="form-group">
+						<label><span>Name</span><?php if($this->session->flashdata('name')){echo '<span class="error">*</span>';}?></label>
+						<input class="form-control" type='text' name='name' placeholder='name'>
+					</div>
+					<div class="form-group">
+						<label><span>Alias</span><?php if($this->session->flashdata('alias')){echo '<span class="error">*</span>';}?></label>
+						<input class="form-control" type='text' name='alias' placeholder='alias'>
+					</div>
+					<div class="form-group">
+						<label><span>Email</span><?php if($this->session->flashdata('email')){echo '<span class="error">*</span>';}?></label>
+						<input class="form-control" type='text' name='email' placeholder='email'>
+					</div>
+					<div class="form-group">
+						<label><span>Password</span><?php if($this->session->flashdata('password')){echo '<span class="error">*</span>';}?></label>
+						<input class="form-control" type='password' name='password'>
+					</div>
+					<div class="form-group">
+						<label><span>Confirm Password</span><?php if($this->session->flashdata('confirm_password')){echo '<span class="error">*</span>';}?></label>
+						<input class="form-control" type='password' name='confirm_password' >
+					</div>
+					<div class="form-group">
+						<button class="btn btn-default" type='submit'>Register</button>
+					</div>
+				</form>
+			</div>
+			<div id='login' class="col-sm-5">
+				<form action='/processes/login' method='post'>
+					<div class="form-group">
+						<label><span>Email</span></label>
+						<input class="form-control" type='email' name='email' placeholder='email'>
+					</div>
+					<div class="form-group">
+						<label><span>Password</span></label>
+						<input class="form-control" type='password' name='password'>
+					</div>
+					<div class="form-group">
+						<button class="btn btn-default" type='submit'>Login</button>
+					</div>	
+				</form>
+			</div>
 		</div>
 	</div>
 </body>
